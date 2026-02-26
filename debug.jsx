9b11e3db950c94674923af3487e10acb436915a9
MrLenh <lenh.nguyenvan@outlook.com>
2026-02-26 09:36:39 +0700

    Add debug.jsx, run.bat, urllist.txt

---
+++ debug.jsx
@@ -0,0 +1,131 @@
+{
+    "input": {
+        "recordID": "recv1rNRPFGVDM",
+            "recordData": {
+            "record_id": "recv1rNRPFGVDM",
+                "record_url": "https://kd2olxcu0u.larksuite.com/record/OgHrrCwcGeLTdecnPcHuvbbOs5c",
+                    "id": "recv1rNRPFGVDM",
+                        "fields": {
+                "Artist": null,
+                    "Mockups": [
+                        {
+                            "size": 320966,
+                            "name": "LHB-5540-PKP__mockup2.jpg",
+                            "file_token": "Ymr0bosEjoCSoOxmBOYuKdoDsHf",
+                            "type": "image/jpeg",
+                            "tmp_url": "https://open.larksuite.com/open-apis/drive/v1/medias/batch_get_tmp_download_url?file_tokens=Ymr0bosEjoCSoOxmBOYuKdoDsHf&extra=%7B%22bitablePerm%22%3A%7B%22tableId%22%3A%22tblTdHQzaqBBeIa1%22%2C%22rev%22%3A132688%7D%7D",
+                            "url": "https://open.larksuite.com/open-apis/drive/v1/medias/Ymr0bosEjoCSoOxmBOYuKdoDsHf/download?extra=%7B%22bitablePerm%22%3A%7B%22tableId%22%3A%22tblTdHQzaqBBeIa1%22%2C%22rev%22%3A132688%7D%7D"
+                        }
+                    ],
+                        "Record ID": [
+                            {
+                                "text": "recv1rNRPFGVDM",
+                                "type": "text"
+                            }
+                        ],
+                            "Occasion": null,
+                                "Design start": 1762425158584,
+                                    "Custom Regular Price": null,
+                                        "Design Completed": 1762505305142,
+                                            "PRODUCT TYPE": "Glass Ornament",
+                                                "Product Code": [
+                                                    {
+                                                        "text": "LHB-5540-PKP",
+                                                        "type": "text"
+                                                    }
+                                                ],
+                                                    "Attachment": [
+                                                        {
+                                                            "size": 371992,
+                                                            "name": "image.png",
+                                                            "file_token": "I7mIbTjT5ohfbKx95KruXvqesMf",
+                                                            "type": "image/png",
+                                                            "tmp_url": "https://open.larksuite.com/open-apis/drive/v1/medias/batch_get_tmp_download_url?file_tokens=I7mIbTjT5ohfbKx95KruXvqesMf&extra=%7B%22bitablePerm%22%3A%7B%22tableId%22%3A%22tblTdHQzaqBBeIa1%22%2C%22rev%22%3A132688%7D%7D",
+                                                            "url": "https://open.larksuite.com/open-apis/drive/v1/medias/I7mIbTjT5ohfbKx95KruXvqesMf/download?extra=%7B%22bitablePerm%22%3A%7B%22tableId%22%3A%22tblTdHQzaqBBeIa1%22%2C%22rev%22%3A132688%7D%7D"
+                                                        },
+                                                        {
+                                                            "size": 286015,
+                                                            "name": "image.png",
+                                                            "file_token": "Cws3bgFKko5JFHxKBNauuuMPseh",
+                                                            "type": "image/png",
+                                                            "tmp_url": "https://open.larksuite.com/open-apis/drive/v1/medias/batch_get_tmp_download_url?file_tokens=Cws3bgFKko5JFHxKBNauuuMPseh&extra=%7B%22bitablePerm%22%3A%7B%22tableId%22%3A%22tblTdHQzaqBBeIa1%22%2C%22rev%22%3A132688%7D%7D",
+                                                            "url": "https://open.larksuite.com/open-apis/drive/v1/medias/Cws3bgFKko5JFHxKBNauuuMPseh/download?extra=%7B%22bitablePerm%22%3A%7B%22tableId%22%3A%22tblTdHQzaqBBeIa1%22%2C%22rev%22%3A132688%7D%7D"
+                                                        }
+                                                    ],
+                                                        "Holiday Date": null,
+                                                            "Niche": null,
+                                                                "Niche OLD": null,
+                                                                    "Personalized": null,
+                                                                        "Product Link 2": null,
+                                                                            "Collections": null,
+                                                                                "Custom Sale Price": null,
+                                                                                    "Product Description": null,
+                                                                                        "Tags": [
+                                                                                            {
+                                                                                                "text": "LHB,PKP,LHB-5540-PKP",
+                                                                                                "type": "text"
+                                                                                            }
+                                                                                        ],
+                                                                                            "Design Link**": {
+                    "link": "https://drive.google.com/drive/folders/1RgCB1rtV5I5BNOzW5OKyYZyPUGZXqdEk?usp=drive_link",
+                        "text": "LHB-5540-PKP"
+                },
+                "PGS Product Types**": "W-Round Glass Ornament",
+                    "Print Areas": "LHB-5540-PKP__fullprint__design",
+                        "Priority": null,
+                            "Stores**": null,
+                                "Task Title": null,
+                                    "Target Audiences": [
+                                        "Couple"
+                                    ],
+                                        "Custom Price Type": null,
+                                            "Etsy Tag": "Engagement Gift\nChristmas Ornament\nGift For Couple\nEngaged Keepsake\nCustom Ornament\nCouples Ornament\nMarried Ornament\nWedding Ornament\nEngaged Ornament\nCouples Gift\nEngaged Gift\nFirst Xmas Engaged\nMinimalist Engaged",
+                                                "Id": "5540",
+                                                    "Trello - Product Type": null,
+                                                        "Description": "lên glass ornament cho couple \n\nlàm viền hoa bên ngoài như hình bên dưới\nlàm 2 option:\n1 viền mang vibe xmas như hình 1\n1 viền binh thường màu trắng như hình 2\n\nquote:\nOur First Christmas \nMarried (nhấn)\nAnna & David \n2025\n\nthêm MK ghi\nOur First Christmas \nEngaged (nhấn)\nAnna & David \n2025\n",
+                                                            "Mr test": null,
+                                                                "Product Title**": "Our First Christmas Engaged Ornament, Engaged Christmas Ornament, Custom Engagement Keepsake, Personalized First Christmas Engaged Ornament\n",
+                                                                    "System Status": "OK",
+                                                                        "PGS Product Code": null,
+                                                                            "Etsy Acc": "CatchyPetals",
+                                                                                "Designer": [
+                                                                                    {
+                                                                                        "avatar_url": "https://s16-imfile-sg.feishucdn.com/static-resource/v1/v3_00oh_f0753435-07c3-4ebd-a2b6-a449611062hu~?image_size=72x72&cut_type=default-face&quality=&format=jpeg&sticker_format=.webp",
+                                                                                        "name": "Been",
+                                                                                        "en_name": "Been",
+                                                                                        "id": "ou_a537ab6573fd7492da2c5a21047084e0",
+                                                                                        "email": "kieuphuong.phan@uoymedia.work"
+                                                                                    }
+                                                                                ],
+                                                                                    "IDEA": [
+                                                                                        {
+                                                                                            "avatar_url": "https://s16-imfile-sg.feishucdn.com/static-resource/v1/v3_00rm_b3220f56-5f00-4587-a7dd-b66cb68843hu~?image_size=72x72&cut_type=default-face&quality=&format=jpeg&sticker_format=.webp",
+                                                                                            "name": "Hảo",
+                                                                                            "en_name": "Hảo _ Idea",
+                                                                                            "id": "ou_d23894683ea94d6990e3547b1ccbfcb0",
+                                                                                            "email": "hao.lethi@uoymedia.work"
+                                                                                        }
+                                                                                    ],
+                                                                                        "System Log": "{\"stores\":[\"The stores field is required.\"]}",
+                                                                                            "TASK TYPE": "Task NEW",
+                                                                                                "Trello - Stores": null,
+                                                                                                    "AI TM check": null,
+                                                                                                        "Check TM/Copryright": "YES",
+                                                                                                            "PGS ID": null,
+                                                                                                                "Board ID": null,
+                                                                                                                    "Date": 1762102800000,
+                                                                                                                        "Product Link": {
+                    "link": "https://www.etsy.com/listing/4400503008/our-first-christmas-engaged-ornament?ref=listings_manager_grid",
+                        "text": "https://www.etsy.com/listing/4400503008/our-first-christmas-engaged-ornament?ref=listings_manager_grid"
+                },
+                "Variants": null,
+                    "Marketplace": "Etsy",
+                        "Holiday": "Christmas",
+                            "Publish Date": 1762512547054,
+                                "Working Status": "Archived",
+                                    "SKU": null
+            }
+        }
+    },
+    "js_code": "function handler(input) {\n  var extraData = {\n    bitablePerm: {\n      tableId:\n        'tblTdHQzaqBBeIa1',\n      attachments: {\n        fldPOiVt2y: {\n          recv1JVeUNaXpL: [\n            'Q5nIbjPHdo4EzAxbduvum8YEsQd',\n          ],\n        },\n      },\n    },\n  };\n  return extraData;\n}\n"
+}
---
+++ run.bat
@@ -0,0 +1,1 @@
+python qualityControl-MR-LAPTOP.py
